91867e8a4f775d2d1a7bbb5e13cffc2748d4f551
Saifullah Ahmed <saifullahahmed380@gmail.com>
2025-08-17 17:01:48 +0500

    prod changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,23 @@
+import { Github } from "lucide-react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <footer className="bg-[#1F262B] border-t border-[#E07A5F33] py-8 mt-16">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-4 px-4">
         <p className="text-[#E0C5A0cc] text-sm tracking-wide">
           © {new Date().getFullYear()}{" "}
-          <span className="text-[#E07A5F] font-semibold">Peak Code Studio</span>. All
-          rights reserved.
+          <span className="text-[#E07A5F] font-semibold">Peak Code Studio</span>
+          . All rights reserved.
         </p>
 
         <div className="flex gap-6">
           {[
-            { label: "GitHub", href: "https://github.com/" },
-            { label: "LinkedIn", href: "https://linkedin.com/" },
+            { label: <FaGithub />, href: "https://github.com/saifullah-max" },
+            {
+              label: <FaLinkedin />,
+              href: "https://www.linkedin.com/in/saifullahahmed2003/",
+            },
           ].map(({ label, href }) => (
             <a
               key={label}
